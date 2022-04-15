839ff8c160ef4c1dec0fc1ba8026224b9cbf6437
Tarsha Simmons <tarsha.simmons04@gmail.com>
2022-04-15 10:43:44 -0600

    Modified models and views

--- products/views.py
+++ products/views.py
@@ -1,8 +1,14 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from .serializers import ProductSerializer
+from .models import Product
 
 @api_view(['GET'])
 def products_list(request):
-    # add query here to get all prodcuts from DB and send with response
 
-    return Response('ok')
+    products = Product.objects.all()
+
+    serializer = ProductSerializer(products, many=True)
+    # add query here to get all prodcuts from DB and send with response
+    
+    return Response(serializer.data)
